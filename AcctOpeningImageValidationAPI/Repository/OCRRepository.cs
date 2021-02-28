339484293781066b3b6c21959a36430c8f441caa
Osinnowo Emmanuel <liquidcoding2009@gmail.com>
2021-02-28 21:05:58 +0100

    Exception Implementation

--- AcctOpeningImageValidationAPI/Repository/OCRRepository.cs
+++ AcctOpeningImageValidationAPI/Repository/OCRRepository.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using AcctOpeningImageValidationAPI.Helpers;
 using AcctOpeningImageValidationAPI.Models;
 using AcctOpeningImageValidationAPI.Repository.Abstraction;
+using HelperLib.Exceptions;
 using Microsoft.Extensions.Options;
 
 namespace AcctOpeningImageValidationAPI.Repository
@@ -20,7 +21,20 @@ namespace AcctOpeningImageValidationAPI.Repository
         {
             var usage = GetAll().Where(x => x.EmailAddress == email);
 
+            if(usage.Count() >= _settings.MaximumUsageForOCR)
+            {
+                throw new MaximumOCRUsageException("You've reached maximum usage for this service, please try again!");
+            }
 
+            if(usage.Count() < _settings.MaximumUsageForOCR)
+            {
+
+            }
+
+            CreateAndReturn(new OCRUsage
+            {
+
+            });
 
             return null;
         }
